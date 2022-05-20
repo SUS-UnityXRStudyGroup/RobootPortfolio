3e6562dcf70ac938389caa7c779498966cba4ef7
SUS-Nakayama-Katsuki <nakayama-k@m.sus-g.co.jp>
2022-05-20 20:05:17 +0900

    スクロール実装とかAbout,PVコンポーネントの追加

--- robootportfolio/src/App.vue
+++ robootportfolio/src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <img alt="Roboot Logo" src="./assets/Roboot.png">
+  </div>
+  <transition>
+    <div class="reveal">
+      <About header="About"/>
+    </div>
+  </transition>
+  <transition>
+    <div class="reveal">
+        <PV header="PV"/>
+    </div>
+  </transition>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import About from './components/About.vue'
+import PV from './components/PromotionVideo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    About,
+    PV
+  },
+
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {      
+      const reveals = document.querySelectorAll(".reveal");
+      for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elTop = reveals[i].getBoundingClientRect().top;
+        const elVisible = 150;
+        if (elTop < windowHeight - elVisible) {
+          reveals[i].classList.add("active");
+        }
+        else{
+          reveals[i].classList.remove("active");
+        }
+      }
+    },
+  },
 }
 </script>
 
 <style>
+body {
+  background-color: #000000;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #51c3c3;
 }
+
+h1{
+  color: #ffffff;
+}
+</style>
+
+<style scoped>
+.reveal{
+  position: relative;
+  opacity: 0;
+  transition: 2s all ease;
+}
+.reveal.active{
+  transform: translateY(0);
+  opacity: 1;
+}
+
+div.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div.container img {
+  max-width: 100%;
+} 
 </style>
---
+++ robootportfolio/src/components/PromotionVideo.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ header }}</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/SNh966U_HoU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</template>
+
+<script>
+export default {
+  name: 'PromotionVideo',
+  props: {
+      header: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
